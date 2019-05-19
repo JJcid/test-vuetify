f6f15a3613ea7582ac0a37206d48c3811000c5e0
JJCid <josejaviercidpinel@gmail.com>
2019-05-19 12:34:28 +0200

    add sidebar drawer

---
+++ src/components/Navbar.vue
@@ -0,0 +1,38 @@
+<template>
+  <nav>
+    <v-toolbar flat app>
+      <v-toolbar-side-icon class="grey--text" @click="showDrawer"></v-toolbar-side-icon>
+      <v-toolbar-title class="text-uppercase grey--text">
+        <span class="font-weight-light">Todo</span>
+        <span>Ninja</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn flat color="grey">
+        <span>Sign Out</span>
+        <v-icon right>exit_to_app</v-icon>
+      </v-btn>
+    </v-toolbar>
+
+    <v-navigation-drawer app v-model="drawer" class="indigo">
+      <p>text</p>
+    </v-navigation-drawer>
+  </nav>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      drawer: false
+    };
+  },
+  methods: {
+    showDrawer() {
+      this.drawer = !this.drawer;
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
