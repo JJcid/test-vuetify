e1a12448dc932b340897d7461b9c077f1e59f5c7
JJCid <josejaviercidpinel@gmail.com>
2019-05-19 12:16:13 +0200

    nav toolbar

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,22 @@
 <template>
-  <v-app>
+  <v-app class="grey lighten-4">
     <v-content>
+      <Navbar/>
       <router-view></router-view>
     </v-content>
   </v-app>
 </template>
 
 <script>
+import Navbar from "./components/Navbar.vue";
+
 export default {
   name: "App",
   data() {
     return {
       //
     };
-  }
+  },
+  components: { Navbar }
 };
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,28 +1,6 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <v-btn class="indigo white--text">click me</v-btn>
-    <v-btn depressed dark color="indigo">click me</v-btn>
-    <v-btn depressed flat color="indigo">click me</v-btn>
-
-    <v-btn depressed class="indigo white--text">
-      <v-icon left>email</v-icon>
-      <span>Send</span>
-    </v-btn>
-
-    <v-btn depressed small class="indigo white--text">
-      <v-icon left small>email</v-icon>
-      <span>Send</span>
-    </v-btn>
-
-    <v-btn depressed large class="indigo white--text">
-      <span>Send</span>
-      <v-icon right large>email</v-icon>
-    </v-btn>
-
-    <v-btn fab depressed small dark color="pink">
-      <v-icon>favorite</v-icon>
-    </v-btn>
   </div>
 </template>
 
