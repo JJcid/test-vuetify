e06eea00ac424d8bd432f6abc2cb9055588b38e0
JJCid <josejaviercidpinel@gmail.com>
2019-05-19 10:40:28 +0200

    vuetify basic styles and fonts

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,23 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Home</h1>
+    <p
+      class="red white--text"
+    >Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione incidunt, aut ut ex quisquam officiis eum natus officia numquam provident consectetur quam distinctio a minus omnis eligendi optio, facilis nisi.</p>
+    <p
+      class="indigo lighten-2 blue--text text--darken-4"
+    >Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequatur, error dolorem at et possimus adipisci neque sint voluptas laudantium doloribus ullam beatae nesciunt numquam quae. Facilis iusto libero cumque repellendus?</p>
+    <h1 class="display-4">Massive Display</h1>
+    <h4 class="display-1">Smaller display</h4>
+    <p class="headline">This is a headline</p>
+    <p class="subheading font-weight-bold">subheading</p>
+    <p class="caption">Caption</p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
+export default {};
 </script>
+
+<style lang="scss" scoped>
+</style>
